2908b109f9a84ac9c0926b3bcfd5fbd48209ac46
vampiregaming15@gmail.com <vampiregaming15@gmail.com>
2023-05-23 16:10:01 +0200

    Ajout d'une bdd json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'formulaire';
+  url = 'http://localhost:3000/users';
+
+  userEmail : string = '';
+  userPassword : string =  '';
+
+  onSubmitForm(form : NgForm){
+    console.log(form.value);
+  }
 }
